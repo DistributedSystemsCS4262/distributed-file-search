6b6f8dd23f74577d976b33c44b8b78e9d4619943
kanchanarp <kanchana.ruwanpathirana@gmail.com>
2017-10-24 11:46:14 +0530

    changes to message and message parser

--- src/p2p_filesharing/Message.java
+++ src/p2p_filesharing/Message.java
@@ -5,10 +5,44 @@
  */
 package p2p_filesharing;
 
+import java.util.Map;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author Scorpius
  */
+
 public class Message {
+    private String message;
+    private MessageClass msgCls;
+    private int msgLength;
+    private Map<String, String> parameters;
+    public Message(){
+        
+    }
+    
+    public Message(String msg){
+        
+    }
+    
+    public Message(MessageClass type, String ip, int port, String name){
+        
+    }
+    
+    public static MessageClass getMessageClass(String message){
+        StringTokenizer strToken=new StringTokenizer(message," ");
+        strToken.nextToken();
+        MessageClass msgCls= MessageClass.valueOf(strToken.nextToken());// = ;
+        return msgCls;
+    }
+    
+    public static int getMessageLength(String message){
+        int length;
+        StringTokenizer strToken=new StringTokenizer(message," ");
+        length= Integer.parseInt(strToken.nextToken());
+        return length;
+    }
+    
     
 }
