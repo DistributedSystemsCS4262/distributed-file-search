2bb0c71a91035476c66227724d95ab9a7e4f5476
hashini <hashini.13@cse.mrt.ac.lk>
2017-11-08 18:11:10 +0530

    simple GUI added (still have to do some small formatting) Node class name changes to UDP

--- src/p2p_filesharing_layered/P2P_filesharing.java
+++ src/p2p_filesharing_layered/P2P_filesharing.java
@@ -1,9 +1,9 @@
 package p2p_filesharing_layered;
 
-import p2p_filesharing_layered.layers.Node;
+import p2p_filesharing_layered.layers.UDP;
 
 public class P2P_filesharing {
     public static void main(String[] args){
-        Node node = new Node();
+        UDP node = new UDP();
     }
 }
